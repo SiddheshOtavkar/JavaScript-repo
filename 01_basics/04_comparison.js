a69c2f17802d0ee945f34fda4e2f15e37180d73c
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-01-25 19:37:42 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+// console.log("2" > 1); // true
+// console.log("02" > 1); //true
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+/* Hence TypeScript is used as it has set of rules */
+
+console.log("2" === 2);
