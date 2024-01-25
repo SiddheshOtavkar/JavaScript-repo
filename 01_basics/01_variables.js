3a6aa81fbad8c7246e5c04e9cf0ac4d40f5aaa68
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-01-25 14:39:17 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553;
+let accoundtEmail = "sid@gmail.com";
+var accountPassword = "12345";
+accountCity = "Mumbai";
+let accountState;
+
+// accountId = 2  --> not allowed
+
+accoundtEmail = "siddhesh@gmail.com";
+accountPassword = "2121212";
+accountCity = "Pune";
+
+console.log(accountId);
+
+console.table([
+  accountId,
+  accoundtEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
