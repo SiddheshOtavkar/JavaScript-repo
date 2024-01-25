ad45b5e633238c72339f54883d9f1214f65ba3d3
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-01-25 19:20:11 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+let name = "siddhesh"; //string datatype
+let age = 20; // number datatype
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/fasle
+// null => it is a standalone value
+// undefined => this we will get when value is not assigned
+// symbol => used for uniqueness
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
