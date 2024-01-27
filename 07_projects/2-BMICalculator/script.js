dfb5833416f78e425299a772e0245a57f47c0067
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-01-27 12:45:49 +0530

    BMI Calc

--- 07_projects/2-BMICalculator/script.js
+++ 07_projects/2-BMICalculator/script.js
@@ -14,6 +14,14 @@ form.addEventListener("submit", function (e) {
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the result
-    results.innerHTML = `<span>${bmi}</span>`;
+    // results.innerHTML = `<span>${bmi}</span>`;
+    // results.innerHTML = `<p></p>`
+    if(bmi < 18.6) {
+        results.innerHTML = `<p>You are under weight ${bmi}</p>`
+    } else if(18.6 < bmi && bmi < 24.9) {
+        results.innerHTML = `<p>You are in normal range ${bmi}</p>`
+    } else {
+        results.innerHTML = `<p>You are overweight ${bmi}</p>`
+    }
   }
 });
